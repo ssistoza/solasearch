6fee6aaeca318c78d368fe6aeb0caeaa86233be1
Shane Sistoza ☕︎ <me@ssistoza.dev>
2026-08-23 13:51:18 -0400

    feat: use nitro

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { varlockVitePlugin } from '@varlock/vite-integration'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [tanstackStart(), viteReact(), tailwindcss(), varlockVitePlugin()],
+  plugins: [tanstackStart(), nitro(), viteReact(), tailwindcss(), varlockVitePlugin()],
 })
 
 export default config
