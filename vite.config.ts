36b041caeb4f56254a106975567264edf5ed8da6
Shane Sistoza ☕︎ <me@ssistoza.dev>
2026-08-23 16:01:06 -0400

    feat: try support varlock in vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { varlockVitePlugin } from '@varlock/vite-integration'
-import { nitro } from 'nitro/vite'
+import { defineConfig } from 'vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { varlockVitePlugin } from '@varlock/vite-integration';
+import { nitro } from 'nitro/vite';
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [tanstackStart(), nitro(), viteReact(), tailwindcss(), varlockVitePlugin()],
-})
+  plugins: [
+    tanstackStart(),
+    nitro(),
+    viteReact(),
+    tailwindcss(),
+    varlockVitePlugin({ ssrInjectMode: 'resolved-env' }),
+  ],
+});
 
-export default config
+export default config;
